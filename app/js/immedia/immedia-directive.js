645fadcf82e094d301028e2e04402cb4977f77c1
Julian Cerruti <jcerruti@creativa77.com>
2015-08-22 13:41:08 -0300

    Work in proress hook to listen for sent messages

--- app/js/immedia/immedia-directive.js
+++ app/js/immedia/immedia-directive.js
@@ -13,4 +13,31 @@ angular.module('immedia', ['immediaControllers'])
       templateUrl: templateUrl('immedia_prescence'),
       controller: 'RoomCtrl'
     };
+  })
+
+  .directive('myImmediaSendMessage', function() {
+    return {
+      restrict: 'AE',
+      link: link
+    };
+
+    function link ($scope, element, attrs) {
+      $scope.$on('ui_message_before_send', function() {
+        console.log('xxx message sent!');
+      });
+      $scope.$on('apiUpdate', function (e, update) {
+        switch (update._) {
+          case 'updateMessageID':
+            console.log('xxx updateMessageID: ' + update.id + ', rnd: ' + update.random_id);
+            break;
+          case 'updateNewMessage':
+            console.log('xxx updateNewMessage: ' + update.message.id);
+            break;
+        }
+      });
+      $scope.$on('history_append', function(e, data) {
+        console.log('xxx history_append: ' + data.messageID);
+      });
+    }
   });
+
