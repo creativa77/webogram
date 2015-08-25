35ae39796771f1c146780184740719c578777953
Julian Cerruti <jcerruti@creativa77.com>
2015-08-24 21:49:37 -0300

    Move immedia presence partial to match default gulp rules. Also fix typo

--- app/js/immedia/immedia-directive.js
+++ app/js/immedia/immedia-directive.js
@@ -10,7 +10,7 @@ angular.module('immedia', ['immediaControllers','immediaServices'])
   .directive('myImmediaPrescence', function() {
     return {
       restrict: 'AE',
-      templateUrl: templateUrl('../immedia_prescence'),
+      templateUrl: '/app/partials/immedia/immedia_presence.html',
       controller: 'RoomCtrl'
     };
   })
